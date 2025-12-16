b74fc23b25924c60e6e0916123795638c747d06a
RAJAT RAI <rairajat280@gmail.com>
2025-12-16 11:58:22 +0530

    Time: 15 ms (34.24%) | Memory: 12.5 MB (10.40%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s = str(x)   # convert integer to string
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
