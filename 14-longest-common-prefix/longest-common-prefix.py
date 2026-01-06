8c774269ac1a842e6258be7c36333d970750f6e6
RAJAT RAI <rairajat280@gmail.com>
2026-01-06 21:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (92.98%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -9,11 +9,9 @@ class Solution(object):
 
         prefix = strs[0]
 
-        for string in strs[1:]:
-    
-            while not string.startswith(prefix):
-                prefix = prefix[:-1]  
-                if not prefix:
-                    return ""  
-        
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""
         return prefix
