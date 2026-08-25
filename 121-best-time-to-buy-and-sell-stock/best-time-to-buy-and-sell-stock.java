1fb0f65d792800f0dcf3fbba4c979ab90aaa0f01
RAJAT RAI <rairajat280@gmail.com>
2026-08-25 12:35:45 +0530

    Time: 1 ms (99.96%) | Memory: 94.5 MB (51.63%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int bestbuy=prices[0];
+        int maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]>bestbuy){
+                maxProfit=Math.max(maxProfit,prices[i]-bestbuy);
+
+            }
+            bestbuy=Math.min(bestbuy,prices[i]);
+        }
+        return maxProfit;
+
+    }
+}
