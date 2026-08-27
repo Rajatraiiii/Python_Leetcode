4371be59f9e223f9ccf164edbcd3f8f1f6e052ff
RAJAT RAI <rairajat280@gmail.com>
2026-08-27 12:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (83.39%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,17 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         
-        for (int i = 0; i < nums.length; i++) {
-            
-            if (target == nums[i]) {
-                return i;
+        int low=0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+
+            }
+            else{
+                low=mid+1;
             }
             
-            if (target < nums[i]) {
-                return i;
-            }
         }
-        
-        return nums.length;
+        return low;
     }
 }
